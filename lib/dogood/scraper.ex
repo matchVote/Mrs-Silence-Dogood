aac61789d7686913cbd98dc622030cdbf08b66dc
jimbonk <jimbonkgit@gmail.com>
2018-03-06 15:34:58 -0500

    Hooks up all publishers for scraping
    * This is completely sequential and synchronous; zero concurrency

--- lib/dogood/scraper.ex
+++ lib/dogood/scraper.ex
@@ -19,15 +19,20 @@ defmodule Dogood.Scraper do
   end
 
   def execute do
-    [pub | _] = _publishers = Dogood.Publishers.active_list()
-    Logger.info("Extracting articles for #{pub.name}")
-    articles = Dogood.Publishers.extract_articles(pub)
-    Logger.info("Consuming #{length(articles)} #{pub.name} articles... (mmmmmm yummy)")
-    Enum.each(articles, &Dogood.Articles.consume/1)
-    Logger.info("Done")
-
+    scrape_publishers(Dogood.Publishers.active_list())
     cooldown()
     execute()
+  end
+
+  def scrape_publishers([]), do: nil
+
+  def scrape_publishers([publisher | remaining_publishers]) do
+    Logger.info("Extracting articles for #{publisher.name}")
+    articles = Dogood.Publishers.extract_articles(publisher)
+    Logger.info("Consuming #{length(articles)} #{publisher.name} articles... (mmmmmm yummy)")
+    Enum.each(articles, &Dogood.Articles.consume/1)
+    Logger.info("Done")
+    scrape_publishers(remaining_publishers)
   end
 
   def cooldown do
