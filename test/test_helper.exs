9b21d482b712eae3db24616f6f9a9f2d23346107
jimbonk <jimbonkgit@gmail.com>
2018-03-06 14:50:47 -0500

    Prevents app from starting when running tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,10 @@
+# Starts all dependencies of dogood without starting dogood itself
+# Used in conjunction with `mix test --no-start`
+Application.load(:dogood)
+for app <- Application.spec(:dogood, :applications) do
+  Application.ensure_all_started(app)
+end
+
 ExUnit.start()
+Dogood.Repo.start_link()
 Ecto.Adapters.SQL.Sandbox.mode(Dogood.Repo, :manual)
