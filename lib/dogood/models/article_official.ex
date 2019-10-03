291a27f06b8d099f560621b6afc6da6819cde8a0
Barrett Helms <barrett.helms@salesloft.com>
2019-10-03 06:49:16 -0400

    Uses articles_officials now

--- lib/dogood/models/article_official.ex
+++ lib/dogood/models/article_official.ex
@@ -1,8 +1,8 @@
 defmodule Dogood.Models.ArticleOfficial do
   use Ecto.Schema
 
-  schema "articles_representatives" do
-    field(:representative_id, :binary_id)
+  schema "articles_officials" do
+    field(:official_id, :binary_id)
     field(:mentioned_count, :integer)
     timestamps(inserted_at: :created_at)
     belongs_to(:article, Dogood.Models.Article)
